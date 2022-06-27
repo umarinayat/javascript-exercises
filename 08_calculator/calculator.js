34262ced117900dedf22cf858e25b6995a1ab429
umar <umarinayat111@gmail.com>
2022-06-28 01:45:40 +0500

    calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(a , b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+ return a - b	
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let total = arr.reduce((a,b) => a + b, 0)
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let total = arr.reduce((a,b) => (a * b))
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  return Math.pow(a,b)
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if(num <= 1){
+    return 1
+  }
+  else {
+    return (num * factorial(num - 1))
+  }
 };
 
 // Do not edit below this line
